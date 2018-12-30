006b69812605587f9a42f99947ba0f7ac654501e
xuexb <fe.xiaowu@gmail.com>
2018-12-30 21:03:22 +0800

    chore: 更新 Node.js 示例

--- elasticsearch-fluentd-kiabana-nginx-node/node/index.js
+++ elasticsearch-fluentd-kiabana-nginx-node/node/index.js
@@ -1,10 +1,12 @@
-const logger = require('fluent-logger');
-
-logger.configure('fluentd.test', {
-    host: '127.0.0.1',
+const logger = require('fluent-logger').createFluentSender('node', {
+    host: 'localhost',
     port: 24224,
     timeout: 3.0,
-    reconnectInterval: 60000, // 10 minutes
+    reconnectInterval: 600000,
 });
 
-logger.emit('follow', { from: 'userA', to: 'userB' });
+logger.emit('follow', {
+    from: 'userA',
+    to: 'userB',
+    t: Date.now(),
+});
